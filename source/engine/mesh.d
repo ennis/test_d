61e8bc7f134306aded0b087667d3f9e1741136aa
Alexandre Bléron <alex.bleron@gmail.com>
2017-06-12 00:01:33 +0200

    progress

--- source/engine/mesh.d
+++ source/engine/mesh.d
@@ -1,9 +1,19 @@
 module engine.mesh;
 import core.types;
+import core.vertex;
 import gfx.buffer;
+import gfx.bind;
+import gfx.state_group;
+import opengl;
 
-struct Mesh(T) if (isVertexType(T))
+struct Mesh(T) if (isVertexType!(T))
 {
+    void bind(ref StateGroup sg)
+    {
+        VertexBuffer(0, vertexBuffer.asSlice(), T.sizeof).bind(sg);
+        IndexBuffer(indexBuffer.asSlice(), GL_UNSIGNED_INT).bind(sg);
+    }
+
     Buffer vertexBuffer;
     Buffer indexBuffer;
     int vertexCount;
